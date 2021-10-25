97cdc3fad97e5005f06565666738e32525747c56
Reed Pan <reedpan@Reeds-MacBook-Pro.local>
2021-10-25 21:32:57 +0800

    add multi_discrete support

--- a2c_ppo_acktr/arguments.py
+++ a2c_ppo_acktr/arguments.py
@@ -75,7 +75,7 @@ def get_args():
     parser.add_argument(
         '--num-processes',
         type=int,
-        default=32, # 16
+        default=2, # 16
         help='how many training CPU processes to use (default: 16)')
     parser.add_argument(
         '--num-steps',
